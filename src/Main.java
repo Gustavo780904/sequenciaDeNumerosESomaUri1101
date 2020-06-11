65253fd3543e56d7175733a2df65a6cf9262059e
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-06-10 21:56:26 -0300

    Projeto finalizado

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,40 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		
-		
+
+		int M = 0, N = 0, ordem = 0;
+
+		do {
+			int soma = 0;
+			M = sc.nextInt();
+			N = sc.nextInt();
+
+			if (M < N) {
+				for (int i = 0; ordem < N; i++) {
+					
+					ordem = M + i;
+					soma = soma + M + i;
+					System.out.print(ordem + " ");
+				}
+				if (M > 0 && N > 0) {
+				System.out.println("Sum=" + soma);
+				}
+			}else {
+				
+				for (int i = 0; ordem < M; i++) {
+					
+					ordem = N + i;
+					soma = soma + N + i;
+					System.out.print(ordem + " ");
+				}
+				if (M > 0 && N > 0) {
+					System.out.println("Sum=" + soma);
+					}
+				
+			}
+
+		} while (N > 0 && M > 0);
+
 		sc.close();
 
 	}
